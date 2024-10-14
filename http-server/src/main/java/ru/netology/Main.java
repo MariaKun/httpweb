0d9aef717508210b8ed0e98b91fe35d4944826d0
Maria Kun <penelopa.alex@yandex.ru>
2024-10-14 22:35:01 +0300

    add

---
+++ http-server/src/main/java/ru/netology/Main.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Server server = new Server();
+        server.run();
+
+    }
+}
+
+
